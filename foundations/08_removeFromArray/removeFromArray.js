aefa9ca0081ece780e4d72d6b344997da80e9616
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-04-01 15:09:04 +1100

    Function removes given arguments from array using filter method

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,12 +1,5 @@
 const removeFromArray = function(arr, ...theArgs) {
-    for (let i = arr.length - 1; i >= 0; i--) {
-        for (let j = 0; j < theArgs.length; j++) {
-            if (arr[i] === theArgs[j]) {
-                arr.splice(i, 1);
-            } 
-        }
-    }
-    return arr;
+    return arr.filter(item => !theArgs.includes(item));
 };
 
 // Do not edit below this line
