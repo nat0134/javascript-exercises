695b1ce8ad1cb55b2f25f3b666b5aea39b9b4210
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-04-01 15:07:27 +1100

    Function removes given arguments from array using nested loops

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        for (let j = 0; j < theArgs.length; j++) {
+            if (arr[i] === theArgs[j]) {
+                arr.splice(i, 1);
+            } 
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
