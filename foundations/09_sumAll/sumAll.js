b45483db8cdedc637f81ec9b2f6c8e18b9c95757
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-04-01 20:13:37 +1100

    Created function summing all integers between two given numbers using loop

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
-
+const sumAll = function(numA, numB) {
+    if (!Number.isFinite(numA) || !Number.isFinite(numB) 
+        || numA < 0 || numB < 0
+        || !Number.isInteger(numA) || !Number.isInteger(numB)) {
+        return "ERROR";
+    }
+    // create two variables smaller & larger and initialize sum
+    let lo = 0;
+    let high = 0;
+    let sum = 0;
+    // if numA < numB then smaller = numA and larger = numB
+    // else smaller = numB and larger = numA
+    if (numA < numB) {
+        lo = numA;
+        high = numB;
+    } else if (numB < numA) {
+        lo = numB;
+        high = numA;
+    }
+    // starting from smaller value to larger value
+    for (let i = lo; i <= high; i++) {
+    // find all values in between and their sum
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
