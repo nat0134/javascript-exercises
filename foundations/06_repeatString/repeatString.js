b6a543e672d11f90fd3033d203e7dd61f160695e
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-04-01 13:49:38 +1100

    Created function reversing strings by converting string to array

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let repeat = "";
+    for (let i = 1; i <= num; i++) {
+        repeat += str;
+    }
+    
+    return repeat;
 };
 
 // Do not edit below this line
