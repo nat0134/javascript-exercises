f2f2bafa155f9cca30f6e2ae9c9d3347e9c8e611
Natalia Lee <z2191119@ad.unsw.edu.au>
2026-04-01 21:11:53 +1100

    Created functions to convert temperatures

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertC = (fahrenheit - 32) * (5/9);
+  if (!Number.isInteger(convertC)) {
+    return Number(convertC.toFixed(1));
+  }
+  return convertC;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertF = celsius * (9/5) + 32; 
+  if (!Number.isInteger(convertF)) {
+    return Number(convertF.toFixed(1));
+  }
+  return convertF;
 };
 
 // Do not edit below this line
